2d64dd9ded994c7ab5e31f6e7415526f29c76b36
Jan Studený <jendas1@yahoo.com>
2022-02-10 10:53:54 +0200

    Minor modifications

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A command-line tool for automatically classifying LCL problems on trees in the polynomial region.",
     long_description=README,
     long_description_content_type="text/markdown",
-    # url="https://github.com/jendas1/rooted-tree-classifier",
+    url="https://github.com/jendas1/poly-classifier",
     author="Jan Studeny",
     author_email="jendas1@yahoo.com",
     license="Unlicense",
